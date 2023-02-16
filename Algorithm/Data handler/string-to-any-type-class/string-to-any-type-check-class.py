798a672cd40295195d2acde0a1984e60703254f7
MathGeeksJay <75050655+40843245@users.noreply.github.com>
2023-02-16 19:56:34 +0800

    Update and rename Algorithm/Data handler/string-to-any-type-class.py to Algorithm/Data handler/string-to-any-type-class/string-to-any-type-check-class.py

--- Algorithm/Data handler/string-to-any-type-class/string-to-any-type-check-class.py
+++ Algorithm/Data handler/string-to-any-type-class/string-to-any-type-check-class.py
@@ -1,8 +1,16 @@
 # File name 
 # string-to-any-type-class.py
 
+## README
+
 # test data for input and status of output shown in the following block.
 # if __name__=='__main__': block.
+
+# It just checkes the string is valid and 
+# Strips the extra tab,newline,whitespace,single quote, double quote, parenthesis,square bracket, curly bracket.
+
+# However, my code is NOT completed.
+# I will complete the code so that it can achieve my goal as soon as possible.
 
 from operator import itemgetter
 
