14ea41e621730d861bbc88d41b01750d52628b8f
MathGeeksJay <75050655+40843245@users.noreply.github.com>
2023-02-16 20:30:15 +0800

    Add files via upload

--- Algorithm/Math problem/Parenthesis Matched/parenthesis-matched-problem-v1/ParenthesisProblem_class.py
+++ Algorithm/Math problem/Parenthesis Matched/parenthesis-matched-problem-v1/ParenthesisProblem_class.py
@@ -1,29 +1,81 @@
+from operator import itemgetter
+
+from Message_ENUM_class import Message_Enum
+from Type_ENUM_class import Type_Enum
+from Bracket_ENUM_class import Bracket_ENUM
+    
+    
 class ParenthesisProblem():
+    
+    def IsParenthesisMatched(self,s1:str,s2:str)->bool:     
+        st=s1+s2
+        pair_s=[ (''.join(Bracket_ENUM.pair[i])) for i in range(0,len(Bracket_ENUM.pair),1)]
+        #print(pair_s)
+        #print(st)
+        pair_set=set(pair_s)
+        st_set=set(st)
+        
+        for p_set in pair_set:
+            intersection_set=st_set.intersection(p_set)
+            #print(intersection_set)
+            if len(st_set)==len(intersection_set):
+                return True
+            
+        return False
+        
     def sort_key1(self,val):
         return val[0]
-            
-    def IsBetweenLine(self,li:list)->bool:
+       
+    def IsBetweenLine_WithList(self,li:list)->bool:
+        
+        if li is None:
+            return False
+        
+        if len(li)==0:
+            return False
+        
+        li.sort(key=self.sort_key1, reverse=False)
         for idx in range(0,len(li),1):
             li_idx=li[idx]
             for jdx in range(idx+1,len(li),1):
                 li_jdx=li[jdx]
                 if (li_jdx[1]-li_idx[1])*(li_jdx[0]-li_idx[0])*(li_jdx[0]-li_idx[1])<0:
-                    print("They are ")
-                    print((li_jdx[1]-li_idx[1]))
-                    print((li_jdx[0]-li_idx[0]))
-                    print((li_jdx[0]-li_idx[1]))
-                    print("respectively.")
                     return True,idx,jdx,li_idx,li_jdx
         return False,-1,-1,[],[]
-
+    
+    # To handle the type such as [(1,1),(2,2)]
+    def IsBetweenLine_WithTupleList(self,li:list,to_consider_same_parenthesis:bool)->bool:
+          
+        if to_consider_same_parenthesis == False:
+            li2=[list(map(itemgetter(0), v)) for v in li]
+            print(li2)
+            result=self.IsBetweenLine_WithList(li2)
+            return result[0]
+        else:
+            
+            if li is None:
+                return False
+            
+            if len(li)==0:
+                return False
+            
+            li.sort(key=self.sort_key1, reverse=False)
+            for idx in range(0,len(li),1):
+                li_idx=li[idx]
+                for jdx in range(idx+1,len(li),1):
+                    li_jdx=li[jdx]
+                    if (li_jdx[1][0]-li_idx[1][0])*(li_jdx[0][0]-li_idx[0][0])*(li_jdx[0][0]-li_idx[1][0])<0:
+                        if self.IsParenthesisMatched(s1=li_jdx[0][1], s2=li_idx[1][1]):
+                            return True
+                        
+            return False
+            
 
     def main(self):
-        for cnt in range(0,10,1):
-            list1=[[30, 46+cnt*cnt], [6+cnt, 19+3*cnt] ]
-
-            list1.sort(key=self.sort_key1, reverse=False)
-
-            result,idx,jdx,li_idx,li_jdx=self.IsBetweenLine(list1)
+        for cnt in range(0,1,1):
+            list1=[[6, 19], [30, 46], [8, 18]]
+            
+            result,idx,jdx,li_idx,li_jdx=self.IsBetweenLine_WithList(list1)
     
             print("start of ------------------------------------------ no."+str(cnt))
     
